8271447b719bc8798eda7bc1575195db3cd7b0c6
ilic90 <ilic90@users.noreply.github.com>
2018-12-27 15:48:34 +0100

    asd

--- app/Category.php
+++ app/Category.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Category extends Model
 {
@@ -26,6 +27,22 @@ class Category extends Model
     public function metaTag()
     {
         return $this->hasOne(MetaTag::class);
+    }
+
+    public function getLiveOffersByCategory($id)
+    {
+        $cat = Category::find($id);
+        $allOffers = $cat->offers;
+        $offers = [];
+        foreach($allOffers as $offer)
+        {
+            if($offer->startDate <= Carbon::now() && $offer->endDate > Carbon::now() || $offer->endDate==null)
+            {
+                array_push($offers,$offer);
+            }
+        }
+        $offers = collect($offers);
+        return $offers;
     }
 
 }
