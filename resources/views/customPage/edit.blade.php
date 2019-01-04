180a353eef7472187a64fccbb5fbd0649aee6bcd
Nikola Obradovic <n.obradovic@hotmail.com>
2019-01-04 12:30:40 +0100

    custom pages admin panel functionality

---
+++ resources/views/customPage/edit.blade.php
@@ -0,0 +1,150 @@
+@extends('adminlte::page')
+
+@section('title', 'Edit CustomPage')
+
+@section('content_header')
+@section('css')
+  <link rel="stylesheet" href="/css/texteditor.css">
+  <link rel="stylesheet" href="/css/displayNone.css">
+@stop
+    
+@stop
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+      <div class="box box-primary">
+        <div class="box-header with-border">
+          <h3 class="box-title">Edit Custom Page</h3>
+        </div>
+        @include('layouts.errors')
+        @include('layouts.messages')
+        <!-- /.box-header -->
+        <!-- form start -->
+        <form role="form"  action="{{ route('update.customPage',['id' => $customPage->id]) }}" method="POST" enctype="multipart/form-data">
+          <div class="box-body">
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control" name="name" value="{{ $customPage->name }}">
+            </div>
+            <div class="form-group" style="display:none">
+              <label for="highlight">Highlight</label>
+              <input type="text" class="form-control" name="highlight" value="{{ $customPage->highlight }}">
+            </div>
+            <div class="form-group">
+              <label for="text">Text</label>
+              <div>
+                  <textarea name="detail" class="textarea" placeholder="Place some text here"
+                            style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">{{ $customPage->text }}</textarea>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="position">Position</label>
+              <input type="text" class="form-control" name="link" value="{{ $customPage->position }}">
+            </div>
+            <div class="form-group">
+            @if($customPage->active == true)
+            <input type="checkbox" name="active" checked="checked"> &nbsp;  <label for="active">Is Active</label>
+            @else
+              <input type="checkbox" name="active" > &nbsp;  <label for="is_default">Is Active</label>
+            @endif
+            </div>
+          </div>
+
+          {!! csrf_field() !!}
+
+          <!-- /.box-body -->
+
+          <div class="box-footer">
+            <button type="submit" class="btn btn-primary">Update Custom Page</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    </div>
+@stop
+@section('js')
+<script src="{{ asset('js/texteditor.js') }}">
+    </script>
+<script>
+  $(function () {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    //CKEDITOR.replace('editor1')
+    //bootstrap WYSIHTML5 - text editor
+    $('.textarea').wysihtml5()
+  })
+</script>
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Datemask dd/mm/yyyy
+    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+    //Datemask2 mm/dd/yyyy
+    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+    //Money Euro
+    $('[data-mask]').inputmask()
+
+    //Date range picker
+    $('#reservation').daterangepicker()
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A' })
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+      {
+        ranges   : {
+          'Today'       : [moment(), moment()],
+          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment().subtract(29, 'days'),
+        endDate  : moment()
+      },
+      function (start, end) {
+        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+      }
+    )
+
+    
+
+    //Date picker
+    $('#datepicker').datepicker({
+      autoclose: true
+    })
+
+    //iCheck for checkbox and radio inputs
+    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+      checkboxClass: 'icheckbox_minimal-blue',
+      radioClass   : 'iradio_minimal-blue'
+    })
+    //Red color scheme for iCheck
+    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+      checkboxClass: 'icheckbox_minimal-red',
+      radioClass   : 'iradio_minimal-red'
+    })
+    //Flat red color scheme for iCheck
+    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+      checkboxClass: 'icheckbox_flat-green',
+      radioClass   : 'iradio_flat-green'
+    })
+
+    //Colorpicker
+    $('.my-colorpicker1').colorpicker()
+    //color picker with addon
+    $('.my-colorpicker2').colorpicker()
+
+    //Timepicker
+    $('.timepicker').timepicker({
+      showInputs: false
+    })
+  })
+
+  
+</script>
+
+@stop
---
+++ resources/views/customPage/index.blade.php
@@ -0,0 +1,60 @@
+@extends('adminlte::page')
+
+@section('title','CustomPages')
+
+@section('content_header')
+@stop
+
+@section('content')
+<div class="row">
+        <div class="col-xs-12">
+
+       
+          <div class="box">
+          
+            <div class="box-header">
+              <h3 class="box-title">All Custom Pages</h3>
+              @include('layouts.messages')
+              @include('layouts.errors')
+              <div class="box-tools">
+
+              
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+            </br>
+              <table class="table table-hover">
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Position</th>
+                  <th>Active</th>
+                  <th>Created At</th>
+                  <th>Updated At</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+                @foreach($customPages as $customPage)
+                <tr>
+                  <td>{{ $customPage->id }}</td>
+                  <td>{{ $customPage->name }}</td>
+                  <td>{{ $customPage->position }</td>
+                  <td>{{ $customPage->active }}</td>
+                  <td>{{ $customPage->created_at->toFormattedDateString() }}</td>
+                  <td>{{ $customPage->updated_at->toFormattedDateString() }}</td>
+                  <td><a href="{{ route('edit.customPage', ['id' => $customPage->id]) }}"><i class="fa fa-pencil"></i></a></td>
+                  <td><a href="{{ route('delete.customPage', ['id' => $customPage->id]) }}"><i class="fa fa-trash"></i></a></td>
+                </tr>
+                @endforeach
+              </table>
+            </div>
+            <!-- /.box-body -->
+
+          </div>
+          
+
+          <!-- /.box -->
+        </div>
+        
+          
+@stop
