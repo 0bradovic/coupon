fb2fb6e8d9398b1a6f1dcdc7fb3db013d6b57286
ilic90 <ilic90@users.noreply.github.com>
2019-09-17 12:51:28 +0200

    css updated

--- resources/views/front/brandOffers.blade.php
+++ resources/views/front/brandOffers.blade.php
@@ -5,7 +5,7 @@
 <div class="category_page">
     <div class="popular_brands_row">
         <span>{{ $brand->name }}</span> 
-        <div class="searchInContent">
+        <div class="searchInContent brand_searchInContent">
             <form action="{{route('search.blade')}}" method="GET" autocomplete="off">
             <label>
                 <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
